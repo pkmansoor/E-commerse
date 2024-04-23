a42fe094bc490416ab198c4fa88eccb21e7cf0b3
Mohammed Mansoor P K <pkmansoor@proton.me>
2024-04-23 15:16:48 +0530

    controller updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,12 +1,17 @@
 const Product = require('../models/productModel')
-exports.getProducts=(req,res, next)=>{
+
+// GET product - /api/v1/products
+exports.getProducts=async(req,res, next)=>{
+     const products = await Product.find()
     res.status(200).json({
         success:true,
-        message:"this route will show all the product indatabase"
+        count:products.length,
+        products
     })
 }
 
-//to create a product - api = {{base-url}}/api/v1/product/new
+
+//to create a product -  /api/v1/product/new
 exports.newProduct=async (req,res,next)=>{
     const product =await Product.create(req.body)
     res.status(201).json({
